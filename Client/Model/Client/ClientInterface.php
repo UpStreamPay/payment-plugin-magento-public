76293c0983c892ea0d6e30fb3499db407ced30bd
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2023-06-09 17:37:38 +0200

    Add ClientInterface.php to manage API calls.

---
+++ Client/Model/Client/ClientInterface.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * UpStream Pay
+ *
+ * Copyright (c) 2019-2023 UpStream Pay.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Claranet France <info@fr.clara.net>
+ */
+
+namespace UpStreamPay\Client\Model\Client;
+
+/**
+ * Interface ClientInterface
+ *
+ */
+interface ClientInterface
+{
+    /**
+     * Get token to authenticate on further API calls.
+     * Return the raw token array without any changes.
+     *
+     * @return array
+     */
+    public function getToken(): array;
+
+    /**
+     * Create UpStream Pay session.
+     * Return the full session without any changes.
+     *
+     * @param array $orderSession
+     *
+     * @return array
+     */
+    public function createSession(array $orderSession): array;
+}
