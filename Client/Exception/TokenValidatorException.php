6e46c5f08d512ae1f0de3439fd74ec8f4d09ab51
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2023-06-13 18:08:54 +0200

    Add Token service to manage token.

---
+++ Client/Exception/TokenValidatorException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * UpStream Pay
+ *
+ * Copyright (c) 2019-2023 UpStream Pay.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Claranet France <info@fr.clara.net>
+ */
+declare(strict_types=1);
+
+namespace UpStreamPay\Client\Exception;
+
+/**
+ * Class TokenValidatorException
+ *
+ * @package UpStreamPay\Client\Exception
+ */
+class TokenValidatorException extends \Exception
+{
+
+}
