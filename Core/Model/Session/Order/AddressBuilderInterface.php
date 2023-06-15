dfa125007f32c025c71001b1c840584c30ae6487
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2023-06-15 17:44:06 +0200

    Add AddressBuilderInterface.

---
+++ Core/Model/Session/Order/AddressBuilderInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * UpStream Pay
+ *
+ * Copyright (c) 2019-2023 UpStream Pay.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Claranet France <info@fr.clara.net>
+ */
+namespace UpStreamPay\Core\Model\Session\Order;
+
+use Magento\Quote\Api\Data\CartInterface;
+
+/**
+ * Interface AddressBuilderInterface
+ *
+ * @package UpStreamPay\Core\Model\Session\Order
+ */
+interface AddressBuilderInterface extends BuilderInterface
+{
+    public const BILLING_ADDRESS = 'billing';
+    public const SHIPPING_ADDRESS = 'shipping';
+
+    /**
+     * Build the Address (shipping or billing) data for the UpStream Pay order.
+     *
+     * @param CartInterface $quote
+     * @param string $addressType
+     *
+     * @return array
+     *
+     * @see UpStream Pay documentation regarding all the fields & format.
+     */
+    public function execute(CartInterface $quote, string $addressType = self::BILLING_ADDRESS): array;
+}
