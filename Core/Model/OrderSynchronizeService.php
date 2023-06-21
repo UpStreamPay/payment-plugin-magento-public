955af190a2f72d4319ed6d12fd222cfd45bb01e1
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2023-06-21 19:01:55 +0200

    Update OrderSynchronizeService.php to create payment & transactions.

--- Core/Model/OrderSynchronizeService.php
+++ Core/Model/OrderSynchronizeService.php
@@ -13,8 +13,14 @@ declare(strict_types=1);
 namespace UpStreamPay\Core\Model;
 
 use Magento\Checkout\Model\Session;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use NoTransactionsException;
 use UpStreamPay\Client\Model\Client\ClientInterface;
-use UpStreamPay\Core\Api\Data\UpStreamPayOrderPaymentInterface;
+use UpStreamPay\Core\Api\Data\OrderPaymentInterface;
+use UpStreamPay\Core\Api\Data\OrderTransactionsInterface;
+use UpStreamPay\Core\Api\OrderPaymentRepositoryInterface;
+use UpStreamPay\Core\Api\OrderTransactionsRepositoryInterface;
 
 /**
  * Class OrderSynchronizeService
@@ -23,52 +29,74 @@ use UpStreamPay\Core\Api\Data\UpStreamPayOrderPaymentInterface;
  */
 class OrderSynchronizeService
 {
+    /**
+     * @param ClientInterface $client
+     * @param OrderPaymentFactory $orderPaymentFactory
+     * @param Session $checkoutSession
+     * @param OrderPaymentRepositoryInterface $orderPaymentRepository
+     * @param OrderTransactions $orderTransactions
+     * @param OrderTransactionsRepositoryInterface $orderTransactionsRepository
+     */
     public function __construct(
         private readonly ClientInterface $client,
-        private readonly UpStreamPayOrderPaymentFactory $orderPaymentFactory,
-        private readonly Session $checkoutSession
+        private readonly OrderPaymentFactory $orderPaymentFactory,
+        private readonly Session $checkoutSession,
+        private readonly OrderPaymentRepositoryInterface $orderPaymentRepository,
+        private readonly OrderTransactions $orderTransactions,
+        private readonly OrderTransactionsRepositoryInterface $orderTransactionsRepository
     ) {
     }
 
     /**
      * Synchronize the UpStream Pay order with Magento custom payment & transaction table.
      *
-     * @param int $quoteId
-     *
      * @return void
+     * @throws LocalizedException
+     * @throws NoTransactionsException
      */
-    public function synchronizeAfterPlaceOrder(int $quoteId): void
+    public function synchronizeAfterPlaceOrder(): void
     {
-        $orderTransactions = $this->client->getAllTransactionsForOrder($quoteId);
         $order = $this->checkoutSession->getLastRealOrder();
+        $quoteId = (int) $order->getQuoteId();
+        $orderId = (int) $order->getEntityId();
 
         if (!$order || !$order->getId()) {
-            throw new \Exception('No order found in session');
+            throw new NoSuchEntityException(__('No order found in session'));
         }
 
-        foreach ($orderTransactions as $orderTransaction) {
-            //Create a row in payment table for each original transaction, it means transactions without a parent_id.
-            if (!isset($orderTransaction['parent_transaction_id'])) {
-                /** @var UpStreamPayOrderPaymentInterface $upstreampayOrderPayment */
-                $upstreampayOrderPayment = $this->orderPaymentFactory->create();
+        $orderTransactionsResponse = $this->client->getAllTransactionsForOrder($quoteId);
 
-                $upstreampayOrderPayment
-                    ->setSessionId($orderTransaction['session_id'])
-                    ->setMethod($orderTransaction['partner'] . ' / ' . $orderTransaction['method'])
+        if (count($orderTransactionsResponse) === 0) {
+            throw new NoTransactionsException('No transactions found after place order with ID ' . $order->getId());
+        }
+
+        foreach ($orderTransactionsResponse as $orderTransactionResponse) {
+            //Create a row in payment table for each original transaction, it means transactions without a parent_id.
+            if (!isset($orderTransactionResponse['parent_transaction_id'])) {
+                //TODO use model to create object & save it.
+                //TODO check we dont have to data in DB before save.
+                /** @var OrderPaymentInterface $orderPayment */
+                $orderPayment = $this->orderPaymentFactory->create();
+
+                $orderPayment
+                    ->setSessionId($orderTransactionResponse['session_id'])
+                    ->setDefaultTransactionId($orderTransactionResponse['id'])
+                    ->setMethod($orderTransactionResponse['partner'] . ' / ' . $orderTransactionResponse['method'])
                     ->setType('primary')
                     ->setQuoteId($quoteId)
-                    ->setOrderId($order->getId())
-                    ->setPaymentId($order->getPayment()->getEntityId())
-                    ->setAmount($orderTransaction['plugin_result']['amount'])
+                    ->setOrderId($orderId)
+                    ->setPaymentId((int) $order->getPayment()->getEntityId())
+                    ->setAmount($orderTransactionResponse['plugin_result']['amount'])
                     ->setAmountCaptured(0.00)
                     ->setAmountRefunded(0.00)
                 ;
 
-
+                $this->orderPaymentRepository->save($orderPayment);
             }
-        }
 
-        //Create transaction based on response.
+            //TODO check we dont have to data in DB before save.
+            $this->orderTransactions->createTransactionFromResponse($orderTransactionResponse, $orderId, $quoteId);
+        }
 
         //Check DB to see if row in custom payment table.
             //Update if needed based on API response.
