9df15604cba824e6eea6ced89a4e9870785c1cbb
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2023-06-22 15:20:30 +0200

    Update the redirect controller.
    - Synchronize & capture.
    - Redirect to checkout success.
    - Restore quote in case of error.
    - Cancel order in case of error.
    - Redirect to cart in case of error.

--- Core/Controller/Payment/ReturnUrl.php
+++ Core/Controller/Payment/ReturnUrl.php
@@ -12,8 +12,13 @@ declare(strict_types=1);
 
 namespace UpStreamPay\Core\Controller\Payment;
 
+use Magento\Checkout\Model\Session;
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Sales\Api\OrderManagementInterface;
+use Psr\Log\LoggerInterface;
+use UpStreamPay\Core\Model\Synchronize\OrderSynchronizeService;
 
 /**
  * Class ReturnUrl
@@ -27,20 +32,42 @@ class ReturnUrl implements HttpGetActionInterface
     public const URL_PATH = 'upstreampay/payment/returnurl';
 
     /**
-     * @param ResultFactory $resultFactory
+     * @param OrderSynchronizeService $orderSynchronizeService
+     * @param Session $checkoutSession
      */
     public function __construct(
-        private readonly ResultFactory $resultFactory
+        private readonly OrderSynchronizeService $orderSynchronizeService,
+        private readonly Session $checkoutSession,
+        private readonly RedirectFactory $redirectFactory,
+        private readonly LoggerInterface $logger,
+        private readonly OrderManagementInterface $orderManagement
     ) {}
 
     /**
-     * @inheritDoc
+     * Handle the redirect coming from UpStream Pay.
+     *
+     * @return ResultInterface
      */
-    public function execute()
+    public function execute(): ResultInterface
     {
-        $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
-        $result->setData(['message' => 'Coming from the returnUrl field after payment.']);
+        $resultRedirect = $this->redirectFactory->create();
 
-        return $result;
+        try {
+            $order = $this->checkoutSession->getLastRealOrder();
+            $this->orderSynchronizeService->synchronizeAndCapture($order);
+
+            $resultRedirect->setPath('checkout/onepage/success', ['_secure' => true]);
+        } catch (\Throwable $exception) {
+            $this->logger->critical('Error while trying to handle the order after redirect from UpStream Pay');
+            $this->logger->critical('Order ID was ' . $order->getEntityId());
+            $this->logger->critical($exception->getMessage(), ['exception' => $exception->getTraceAsString()]);
+
+            //Restore the user quote & redirect to cart.
+            $this->checkoutSession->restoreQuote();
+            $this->orderManagement->cancel($order->getEntityId());
+            $resultRedirect->setPath('checkout/cart', ['_secure' => true]);
+        }
+
+        return $resultRedirect;
     }
 }
