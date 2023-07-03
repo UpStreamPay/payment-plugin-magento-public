0dadda575bf7c57a4fa140eb17996224b3663665
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2023-07-03 15:20:21 +0200

    Add admin grid for payments & transactions.

---
+++ Core/Controller/Adminhtml/Payments/Index.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * UpStream Pay
+ *
+ * Copyright (c) 2019-2023 UpStream Pay.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Claranet France <info@fr.clara.net>
+ */
+declare(strict_types=1);
+
+namespace UpStreamPay\Core\Controller\Adminhtml\Payments;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+/**
+ * Class Index
+ *
+ * @package UpStreamPay\Core\Controller\Adminhtml\Payments
+ */
+class Index extends Action implements HttpGetActionInterface
+{
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        private readonly PageFactory $pageFactory
+    ) {
+        parent::__construct($context);
+    }
+
+    /**
+     * Add the main Admin Grid page
+     *
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        $resultPage = $this->pageFactory->create();
+        $resultPage->setActiveMenu('UpStreamPay_Core::payments');
+        $resultPage->getConfig()->getTitle()->prepend(__('Payments grid'));
+
+        return $resultPage;
+    }
+}
