6741972c9fcbb9135585c4855bd206cb020f7b18
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2023-07-04 15:02:41 +0200

    When creating a session check if we have to update or create a payment method.

--- Core/Model/Session.php
+++ Core/Model/Session.php
@@ -33,12 +33,14 @@ class Session implements SessionInterface
      * @param OrderService $orderService
      * @param CheckoutSession $checkoutSession
      * @param LoggerInterface $logger
+     * @param PaymentMethod $paymentMethod
      */
     public function __construct(
         private readonly ClientInterface $client,
         private readonly OrderService $orderService,
         private readonly CheckoutSession $checkoutSession,
-        private readonly LoggerInterface $logger
+        private readonly LoggerInterface $logger,
+        private readonly PaymentMethod $paymentMethod
     ) {
     }
 
@@ -62,6 +64,23 @@ class Session implements SessionInterface
 
         $order = $this->orderService->execute($quote);
         $response = $this->client->createSession($order);
+
+        //Save payment method from session in DB.
+        foreach ($response['protocols'] as $partnerName => $method) {
+            foreach ($method as $methodName => $methodValues) {
+                $labels = $methodValues['configurations']['labels'];
+
+                if (in_array(PaymentMethod::PRIMARY, $labels)) {
+                    $type = PaymentMethod::PRIMARY;
+                } elseif (in_array(PaymentMethod::SECONDARY, $labels)) {
+                    $type = PaymentMethod::SECONDARY;
+                }
+
+                $paymentMethodName = $partnerName . ' / ' . $methodName;
+
+                $this->paymentMethod->updateOrCreate($paymentMethodName, $type);
+            }
+        }
 
         return [$response];
     }
