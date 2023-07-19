f4e52e5d07d39b985b43223820465f1ebeb31916
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2023-07-19 17:24:40 +0200

    Disable core event to send invoice email after place order.
    Add custom observer to disable emails in case payment method is upstream_pay.

---
+++ Core/Observer/SendUpStreamPayInvoiceEmailObserver.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * UpStream Pay
+ *
+ * Copyright (c) 2023 UpStream Pay.
+ * This file is open source and available under the BSD 3 license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Claranet France <info@fr.clara.net>
+ */
+declare(strict_types=1);
+
+namespace UpStreamPay\Core\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Quote\Model\Quote;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Email\Container\InvoiceIdentity;
+use Magento\Sales\Model\Order\Email\Sender\InvoiceSender;
+use Psr\Log\LoggerInterface;
+use UpStreamPay\Core\Model\Config;
+
+/**
+ * Class SendUpStreamPayInvoiceEmailObserver
+ *
+ * @package UpStreamPay\Core\Observer
+ */
+class SendUpStreamPayInvoiceEmailObserver implements ObserverInterface
+{
+    /**
+     * @param LoggerInterface $logger
+     * @param InvoiceSender $invoiceSender
+     * @param InvoiceIdentity $invoiceIdentity
+     */
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly InvoiceSender $invoiceSender,
+        private readonly InvoiceIdentity $invoiceIdentity
+    ) {
+    }
+
+    /**
+     * Send invoice email if allowed.
+     *
+     * @see sendEmailInvoiceUpstream
+     *
+     * @param Observer $observer
+     *
+     * @return void
+     */
+    public function execute(Observer $observer): void
+    {
+        if (!$this->isInvoiceEmailAllowed()) {
+            return;
+        }
+
+        /** @var  Quote $quote */
+        $quote = $observer->getEvent()->getQuote();
+        /** @var  Order $order */
+        $order = $observer->getEvent()->getOrder();
+
+        /**
+         * a flag to set that there will be redirect to third party after confirmation
+         */
+        $redirectUrl = $quote->getPayment()->getOrderPlaceRedirectUrl();
+        if (!$redirectUrl && $order->getCanSendNewEmailFlag()) {
+            try {
+                $invoice = current($order->getInvoiceCollection()->getItems());
+                if ($invoice) {
+                    //Add this condition to block emails only if using upstream pay.
+                    if ($order->getPayment()->getMethod() !== Config::METHOD_CODE_UPSTREAM_PAY) {
+                        $this->invoiceSender->send($invoice);
+                    }
+                }
+            } catch (\Throwable $e) {
+                $this->logger->critical($e);
+            }
+        }
+    }
+
+    /**
+     * Is invoice email sending enabled
+     *
+     * @return bool
+     */
+    private function isInvoiceEmailAllowed(): bool
+    {
+        return $this->invoiceIdentity->isEnabled();
+    }
+}
