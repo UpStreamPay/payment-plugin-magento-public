0c4d9ef02a1dfe7f5d3dbda0508e5b0f264cfffa
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2023-08-21 12:04:19 +0200

    Use dedicated exception & throw exception in case of create session error.

--- Core/Model/Session.php
+++ Core/Model/Session.php
@@ -20,6 +20,7 @@ use Psr\Log\LoggerInterface;
 use Throwable;
 use UpStreamPay\Client\Model\Client\ClientInterface;
 use UpStreamPay\Core\Api\SessionInterface;
+use UpStreamPay\Core\Exception\CreateSessionException;
 use UpStreamPay\Core\Model\Session\Order\OrderService;
 
 /**
@@ -49,6 +50,7 @@ class Session implements SessionInterface
      * Return the session data (build the order) in order to indicate to UpStream Pay what payment methods to return.
      *
      * @return array
+     * @throws CreateSessionException
      * @throws LocalizedException
      * @throws NoSuchEntityException
      * @throws Exception
@@ -61,7 +63,7 @@ class Session implements SessionInterface
             $errorMessage = 'Tried to retrieve a quote to create UpStream Pay session but it appears it is invalid.';
             $this->logger->critical($errorMessage);
 
-            throw new Exception($errorMessage);
+            throw new CreateSessionException($errorMessage);
         }
 
         $order = $this->orderService->execute($quote);
@@ -71,6 +73,8 @@ class Session implements SessionInterface
         } catch (Throwable $exception) {
             $this->logger->critical('Error while creating the session, widget cannot be displayed.');
             $this->logger->critical($exception->getMessage(), ['exception' => $exception->getTraceAsString()]);
+
+            throw new CreateSessionException($exception->getMessage());
         }
 
         //Save payment method from session in DB.
