c7d27a5c34be94892b4f8dd80824487eb380d3bd
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2023-12-04 16:52:08 +0100

    [UP-111] Display wallet widget in customer account.

---
+++ Core/view/frontend/requirejs-config.js
@@ -0,0 +1,18 @@
+
+/*
+ * UpStream Pay
+ *
+ * Copyright (c) 2023 UpStream Pay.
+ * This file is open source and available under the BSD 3 license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Claranet France <info@fr.clara.net>
+ */
+
+var config = {
+    map: {
+        '*': {
+            purseWallet: 'UpStreamPay_Core/js/view/customer/purse-wallet'
+        }
+    }
+};
