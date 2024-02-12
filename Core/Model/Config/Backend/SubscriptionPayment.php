7798e4a7ae7a492714236c6e91c52e67b30f9928
pingueta <pingueta@pglw0216.lild01.pictime.fr>
2024-02-12 09:18:12 +0100

    feat: UP-130
    mise en place de l'interface admin

---
+++ Core/Model/Config/Backend/SubscriptionPayment.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * UpStream Pay
+ *
+ * Copyright (c) 2023 UpStream Pay.
+ * This file is open source and available under the BSD 3 license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Claranet France <info@fr.clara.net>
+ */
+declare(strict_types=1);
+
+namespace UpStreamPay\Core\Model\Config\Backend;
+
+use Magento\Cron\Model\Config\Source\Frequency;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Config\Value;
+use Magento\Framework\App\Config\ValueFactory;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
+
+class SubscriptionPayment extends Value
+{
+
+    public const CRON_STRING_PATH = 'payment/upstream_pay/subscription_payment/frequency';
+
+    /**
+     * Cron mode path
+     */
+    public const CRON_MODEL_PATH = 'payment/upstream_pay/subscription_payment/run/model';
+
+    /**
+     * @param Context $context
+     * @param Registry $registry
+     * @param ScopeConfigInterface $config
+     * @param TypeListInterface $cacheTypeList
+     * @param ValueFactory $configValueFactory
+     * @param AbstractResource $resource
+     * @param AbstractDb $resourceCollection
+     * @param string $runModelPath
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        ScopeConfigInterface $config,
+        TypeListInterface $cacheTypeList,
+        private readonly ValueFactory $configValueFactory,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        private readonly string $runModelPath = '',
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
+    }
+
+    /**
+     * After save handler
+     *
+     * @return $this
+     * @throws LocalizedException
+     */
+    public function afterSave()
+    {
+        $time = $this->getData('groups/generate/fields/time/value') ?:
+            explode(
+                ',',
+                $this->_config->getValue('subscription_payment/generate/time', $this->getScope(), $this->getScopeId()) ?: '0,0,0'
+            );
+        $frequency = $this->getValue();
+
+        $cronExprArray = [
+            (int)($time[1] ?? 0), //Minute
+            (int)($time[0] ?? 0), //Hour
+            $frequency == Frequency::CRON_MONTHLY ? '1' : '*', //Day of the Month
+            '*', //Month of the Year
+            $frequency == Frequency::CRON_WEEKLY ? '1' : '*', //# Day of the Week
+        ];
+
+        $cronExprString = join(' ', $cronExprArray);
+
+        try {
+            $this->configValueFactory->create()->load(
+                self::CRON_STRING_PATH,
+                'path'
+            )->setValue(
+                $cronExprString
+            )->setPath(
+                self::CRON_STRING_PATH
+            )->save();
+            $this->configValueFactory->create()->load(
+                self::CRON_MODEL_PATH,
+                'path'
+            )->setValue(
+                $this->runModelPath
+            )->setPath(
+                self::CRON_MODEL_PATH
+            )->save();
+        } catch (\Exception $e) {
+            throw new LocalizedException(__('We can\'t save the cron expression.'));
+        }
+        return parent::afterSave();
+    }
+}
