30e52a40b406f9c549cc19917c4bb1e7877c93ff
Raemdonck Philippe <philippe.raemdonck@pictime-groupe.com>
2024-03-15 17:41:38 +0100

    (feat/UP-143): Retry subscription payment.

--- Core/Model/Subscription/RetryService.php
+++ Core/Model/Subscription/RetryService.php
@@ -15,14 +15,18 @@ namespace UpStreamPay\Core\Model\Subscription;
 
 use Magento\Framework\Event\ManagerInterface;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Sales\Api\InvoiceRepositoryInterface;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
-use Magento\Sales\Model\Order\Payment\Processor;
 use Psr\Log\LoggerInterface;
 use UpStreamPay\Core\Api\Data\SubscriptionInterface;
 use UpStreamPay\Core\Api\Data\SubscriptionRetryInterface;
+use UpStreamPay\Core\Api\OrderPaymentRepositoryInterface;
+use UpStreamPay\Core\Api\OrderTransactionsRepositoryInterface;
 use UpStreamPay\Core\Api\SubscriptionRepositoryInterface;
 use UpStreamPay\Core\Api\SubscriptionRetryRepositoryInterface;
+use UpStreamPay\Core\Model\Actions\CaptureDuplicateService;
+use UpStreamPay\Core\Model\Actions\DuplicateService;
 use UpStreamPay\Core\Model\Config;
 use UpStreamPay\Core\Model\OrderTransactions;
 use UpStreamPay\Core\Model\Subscription;
@@ -35,113 +39,135 @@ use UpStreamPay\Core\Model\SubscriptionRetry;
  */
 class RetryService
 {
+    /**
+     * @param Config $config
+     * @param SubscriptionRetryRepositoryInterface $subscriptionRetryRepository
+     * @param OrderRepositoryInterface $orderRepository
+     * @param SubscriptionRepositoryInterface $subscriptionRepository
+     * @param LoggerInterface $logger
+     * @param ManagerInterface $eventManager
+     * @param DuplicateService $duplicateService
+     * @param CartRepositoryInterface $cartRepository
+     * @param CaptureDuplicateService $captureDuplicateService
+     * @param OrderTransactionsRepositoryInterface $orderTransactionsRepository
+     * @param OrderPaymentRepositoryInterface $orderPaymentRepository
+     */
     public function __construct(
         private readonly Config $config,
         private readonly SubscriptionRetryRepositoryInterface $subscriptionRetryRepository,
-        private readonly Processor $paymentProcessor,
         private readonly OrderRepositoryInterface $orderRepository,
         private readonly SubscriptionRepositoryInterface $subscriptionRepository,
-        private readonly InvoiceRepositoryInterface $invoiceRepository,
         private readonly LoggerInterface $logger,
-        private readonly ManagerInterface $eventManager
+        private readonly ManagerInterface $eventManager,
+        private readonly DuplicateService $duplicateService,
+        private readonly CartRepositoryInterface $cartRepository,
+        private readonly CaptureDuplicateService $captureDuplicateService,
+        private readonly OrderTransactionsRepositoryInterface $orderTransactionsRepository,
+        private readonly OrderPaymentRepositoryInterface $orderPaymentRepository
     )
     {
     }
 
     public function execute(SubscriptionRetryInterface $subscriptionRetry): void
     {
-        $subscriptionToRetry = $this->subscriptionRepository->getById($subscriptionRetry->getSubscriptionId());
+        $numberOfRetries = $subscriptionRetry->getNumberOfRetries() + 1;
 
-        if ($subscriptionRetry->canBeRetried()) {
+        try {
+            //First thing we do is update the number of retry, doesn't matter if we actually tried something or not.
+            $subscriptionRetry->setNumberOfRetries($numberOfRetries);
+            $this->subscriptionRetryRepository->save($subscriptionRetry);
+            $subscriptionToRetry = $this->subscriptionRepository->getById($subscriptionRetry->getSubscriptionId());
+            $parentSubscriptionToRetry = $this->subscriptionRepository->getParentSubscription($subscriptionToRetry);
             $order = $this->orderRepository->get($subscriptionToRetry->getOrderId());
-            $payment = $order->getPayment();
-            $payment->setIsRetry(true);
-            $invoice = $this->invoiceRepository->get($subscriptionToRetry->getInvoiceId());
 
-            if ($subscriptionRetry->getRetryType() === OrderTransactions::AUTHORIZE_ACTION) {
-                //TODO
-                //Call DuplicateService (need UP-132)
-                //We need a retrieve a way to know the status of the duplicate, so the the DuplicateService needs
-                //to return the proper data.
-                //Based on the status, we have actions to do.
-                //- success => save the retry status (duplicate service triggers capture or UP-132 creates a service or something to capture after a duplicate).
-                //- waiting => save the retry status & do nothing else.
-                //- error => save the retry status & check if we reached max number of retry. If so cancel everything (order, subscription etc).
-                $status = '';
-            } else {
-                try {
-                    $payment = $this->paymentProcessor->capture($payment, $invoice);
-
-                    //After capture is done trigger pay of the invoice.
-                    if ($invoice->getIsPaid()) {
-                        $invoice->pay();
-                        $status = SubscriptionRetry::SUCCESS_STATUS;
-                    } else {
-                        if ($payment->getIsRetryInError()) {
-                            $status = SubscriptionRetry::ERROR_STATUS;
-                        } else {
-                            $status = SubscriptionRetry::WAITING_STATUS;
-                        }
-                    }
-                } catch (\Throwable $exception) {
-                    //Exception during capture process means we have an error.
-                    $status = SubscriptionRetry::ERROR_STATUS;
-
-                    $this->logger->error(
-                        "Error while retrying payment of subscription with ID: " . $subscriptionToRetry->getEntityId()
+            if ($subscriptionRetry->canBeRetried()) {
+                if ($subscriptionRetry->getRetryType() === OrderTransactions::AUTHORIZE_ACTION) {
+                    $quote = $this->cartRepository->get((int)$order->getQuoteId());
+                    $this->duplicateService->execute(
+                        $quote,
+                        $subscriptionRetry->getTransactionId(),
+                        $order,
+                        $subscriptionToRetry,
+                        $parentSubscriptionToRetry,
+                        null,
+                        true
                     );
-                    $this->logger->error($exception->getMessage(), ['exception' => $exception->getTraceAsString()]);
+                } else {
+                    $duplicateAuthorize = $this->orderTransactionsRepository->getByTransactionId(
+                        $subscriptionRetry->getTransactionId()
+                    );
+                    $orderPayment = $this->orderPaymentRepository->getById($duplicateAuthorize->getParentPaymentId());
+
+                    $this->captureDuplicateService->execute(
+                        $order,
+                        $orderPayment,
+                        $subscriptionToRetry,
+                        $duplicateAuthorize
+                    );
+                }
+            } else {
+                if ($subscriptionToRetry->getSubscriptionStatus() !== Subscription::CANCELED) {
+                    $this->cancelSubscription($subscriptionToRetry, $parentSubscriptionToRetry);
+                }
+
+                if ($subscriptionRetry->getRetryStatus() !== SubscriptionRetry::FAILURE_STATUS) {
+                    $subscriptionRetry->setRetryStatus(SubscriptionRetry::FAILURE_STATUS);
+                    $this->subscriptionRetryRepository->save($subscriptionRetry);
+                }
+
+                $this->cancelOrder($order);
+            }
+        } catch (\Throwable $exception) {
+            if ($numberOfRetries >= $this->config->getSubscriptionPaymentMaximumPaymentRetry()) {
+                $subscriptionRetry->setRetryStatus(SubscriptionRetry::FAILURE_STATUS);
+                $this->subscriptionRetryRepository->save($subscriptionRetry);
+
+                if ($subscriptionToRetry && $subscriptionToRetry->getEntityId()) {
+                    $this->cancelSubscription($subscriptionToRetry, $parentSubscriptionToRetry);
+                } else {
+                    $this->logger->critical(
+                        sprintf(
+                            'Subscription with ID %s must be canceled manually after retry failure.',
+                            $subscriptionRetry->getSubscriptionId()
+                        )
+                    );
+                }
+
+                if ($order && $order->getEntityId()) {
+                    $this->cancelOrder($order);
+                } else {
+                    $this->logger->critical(
+                        sprintf(
+                            'Order linked to subscription ID %s must be canceled manually after retry failure.',
+                            $subscriptionRetry->getSubscriptionId()
+                        )
+                    );
                 }
             }
 
-            $numberOfRetries = $subscriptionRetry->getNumberOfRetries() + 1;
-
-            if ($numberOfRetries >= $this->config->getSubscriptionPaymentMaximumPaymentRetry()
-                && $status === OrderTransactions::ERROR_STATUS) {
-                $status = SubscriptionRetry::FAILURE_STATUS;
-
-                $this->cancelSubscription($subscriptionToRetry);
-                $this->cancelOrder();
-            }
-
-            $subscriptionRetry
-                ->setNumberOfRetries($numberOfRetries)
-                ->setRetryStatus($status)
-            ;
-
-            $this->subscriptionRetryRepository->save($subscriptionRetry);
-
-            $this->eventManager->dispatch(
-                'usp_subscription_retry_success',
-                [
-                    'subscription_id' => $subscriptionToRetry->getEntityId(),
-                    'subscription_identifier' => $subscriptionToRetry->getSubscriptionIdentifier(),
-                ]
+            $this->logger->error(
+                "Error while retrying payment of subscription with ID: " . $subscriptionToRetry->getEntityId()
             );
-        } else {
-            //We should never enter this condition because we trigger cancel & failure after a retry if needed
-            //But just in case for safety cancel the retry & subscription if needed.
-            if ($subscriptionRetry->getNumberOfRetries() >= $this->config->getSubscriptionPaymentMaximumPaymentRetry()
-                && $subscriptionRetry->getRetryStatus() === OrderTransactions::ERROR_STATUS) {
-                $this->cancelSubscription($subscriptionToRetry);
-
-                $subscriptionRetry->setRetryStatus(SubscriptionRetry::FAILURE_STATUS);
-                $this->subscriptionRetryRepository->save($subscriptionRetry);
-            }
+            $this->logger->error($exception->getMessage(), ['exception' => $exception->getTraceAsString()]);
         }
     }
 
     /**
+     * Cancel the subscription & expire the parent subscription.
+     *
      * @param SubscriptionInterface $subscriptionToRetry
+     * @param SubscriptionInterface $parentSubscription
      *
      * @return void
      * @throws LocalizedException
      */
-    private function cancelSubscription(SubscriptionInterface $subscriptionToRetry): void
+    private function cancelSubscription(SubscriptionInterface $subscriptionToRetry, SubscriptionInterface $parentSubscription): void
     {
         $subscriptionToRetry
             ->setSubscriptionStatus(Subscription::CANCELED)
-            ->setPaymentStatus(Subscription::ERROR);
+            ->setPaymentStatus(Subscription::ERROR)
+            ->setNextPaymentDate(null)
+        ;
         $this->subscriptionRepository->save($subscriptionToRetry);
         $this->eventManager->dispatch(
             'usp_subscription_canceled',
@@ -150,5 +176,32 @@ class RetryService
                 'subscription_identifier' => $subscriptionToRetry->getSubscriptionIdentifier(),
             ]
         );
+
+        $parentSubscription->setSubscriptionStatus(Subscription::EXPIRED);
+        $this->subscriptionRepository->save($parentSubscription);
+        $this->eventManager->dispatch(
+            'usp_subscription_canceled',
+            [
+                'subscription_id' => $parentSubscription->getEntityId(),
+                'subscription_identifier' => $parentSubscription->getSubscriptionIdentifier(),
+            ]
+        );
+    }
+
+    /**
+     * @param OrderInterface $order
+     *
+     * @return void
+     * @throws LocalizedException
+     */
+    private function cancelOrder(OrderInterface $order): void
+    {
+        if ($order->canCancel()) {
+            $order->cancel();
+        } else {
+            $order->getPayment()->deny();
+        }
+
+        $this->orderRepository->save($order);
     }
 }
